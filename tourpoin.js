7529698f085c640d20ef7692fcad3744e0ef1fd6
aomkul <124546386+aomkul@users.noreply.github.com>
2023-05-29 12:05:39 +0700

    Add files via upload

---
+++ tourpoin.js
@@ -0,0 +1,156 @@
+var tourpoint=
+{
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ต๋อง เต็ม โต๊ะ",
+          "level": 10,
+          "type": "restaurant"
+        },
+        "geometry": {
+          "coordinates": [
+            98.96770254232689,
+            18.796641752099873
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "อรรถรส",
+          "level": 8,
+          "type": "restaurant"
+        },
+        "geometry": {
+          "coordinates": [
+            100.57162754232758,
+            13.737604962372615
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ลูกไก่ทอง",
+          "level": 8,
+          "type": "restaurant"
+        },
+        "geometry": {
+          "coordinates": [
+            100.51120000000066,
+            13.726735027736197
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ตู้กับข้าว",
+          "level": 9,
+          "type": "restaurant"
+        },
+        "geometry": {
+          "coordinates": [
+            98.38800636572864,
+            7.883705779794383
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เสวย",
+          "level": 10,
+          "type": "restaurant"
+        },
+        "geometry": {
+          "coordinates": [
+            100.48868178043568,
+            13.755323625404017
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "อีสานจิ้มจุ่ม",
+          "level": 7,
+          "type": "restaurant"
+        },
+        "geometry": {
+          "coordinates": [
+            100.54302700000056,
+            13.91889098647431
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ไจ๊ บะหมี่เกี๊ยวกุ้งสะพานเหลือง ",
+          "level": 9,
+          "type": "restaurant"
+        },
+        "geometry": {
+          "coordinates": [
+            100.52207489814549,
+            13.732220448730772
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ประจักษ์ เป็ดย่าง",
+          "level": 8,
+          "type": "restaurant"
+        },
+        "geometry": {
+          "coordinates": [
+            100.51606374603477,
+            13.72031926609813
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "กะเพราขุนช้าง",
+          "level": 6,
+          "type": "restaurant"
+        },
+        "geometry": {
+          "coordinates": [
+            100.59182205733879,
+            13.826981161768344
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ออน ล๊อก หยุ่น",
+          "level": 6,
+          "type": "restaurant"
+        },
+        "geometry": {
+          "coordinates": [
+            100.5006524091408,
+            13.746974745743273
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
